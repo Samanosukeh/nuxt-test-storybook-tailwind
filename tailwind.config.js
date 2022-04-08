4bb95b15b927e7a3925a12eaccc29538316d96d4
Flauberth Duarte <flauberth.duarte@65a.com.br>
2022-04-08 17:36:50 -0300

    .

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      blue: '#148DD9',
+      red: '#e80c00',
+      gray: '#a6a6a6',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
